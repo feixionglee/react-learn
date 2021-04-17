501579b49021f687ff6db6eb2c5d9310ee9bdaec
Feixiong Li <fli@tigerconnect.com>
2021-04-18 00:03:34 +0800

    explanation the setState in reducer

--- src/Counter.js
+++ src/Counter.js
@@ -1,3 +1,8 @@
+// This code is from https://medium.com/@dan_abramov/you-might-not-need-redux-be46360cf367
+//
+// Below is explanation why `increment` or `decrement` works
+// https://stackoverflow.com/questions/39806802/lifecycle-event-state-and-prevstate-in-react-js/39806997#
+
 import React, { Component } from 'react';
 
 class Counter extends Component {
